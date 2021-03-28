0332166c72cf382e253def0b4ea60c78d87c9c97
Jjungs7 <jeongy95@gmail.com>
2021-03-28 22:44:37 +0900

    install input needed packages first

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap AdoptOpenJDK/openjdk
 
+brew install --cask adoptopenjdk
+brew install direnv pyenv nvm
+
 brew install tree tmux
 brew install --cask iterm2
 
 brew install zsh-autosuggestions zsh-syntax-highlighting
-
-brew install direnv pyenv nvm
-brew install --cask adoptopenjdk
 
 brew install --cask intellij-idea visual-studio-code
 
@@ -24,9 +24,6 @@ brew install --cask firefox google-chrome
 
 brew install --cask discord
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sed -i '' "s/prompt_segment blue "'$CURRENT_FG'" '%~'/prompt_segment blue "'$CURRENT_FG'" '%1d'/" ~/.oh-my-zsh/themes/agnoster.zsh-theme
-
 # powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
@@ -34,6 +31,9 @@ cd fonts
 cd ..
 rm -rf fonts
 #Ubuntu Mono Derivative Powerline
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i '' "s/prompt_segment blue "'$CURRENT_FG'" '%~'/prompt_segment blue "'$CURRENT_FG'" '%1d'/" ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
 cp zshrc ~/.zshrc
 cp vimrc ~/.vimrc
